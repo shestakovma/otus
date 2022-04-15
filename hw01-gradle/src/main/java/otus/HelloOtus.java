49733bd9ba67231fd69456fdca48d2d1a04fee78
shestakovma <100130780+shestakovma@users.noreply.github.com>
2022-04-15 11:03:38 +0300

    hw01-gradle

---
+++ hw01-gradle/src/main/java/otus/HelloOtus.java
@@ -0,0 +1,8 @@
+package otus;
+import com.google.common.math.IntMath;
+
+public class HelloOtus {
+    public static void GuavaExampleIntMath_checkedAdd(int a, int b) {
+       System.out.println(IntMath.checkedAdd(a, b));
+    }
+}
